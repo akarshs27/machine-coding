9ce9824ef77d2495af7e6e8b2fbafe2f279f0051
Akarsh Srivastava <akarshs27@gmail.com>
2023-08-19 05:08:26 +0000

    Infinite scroll

--- react/infinite-scroll/src/App.js
+++ react/infinite-scroll/src/App.js
@@ -5,42 +5,50 @@ import './App.css';
 function App() {
 
   const [items,setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(0);
 
 
   async function getItemsList() {
+    setIsLoading(true);
     try {
-      const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page}`);
+      const res = await fetch(`https://api.slingacademy.com/v1/sample-data/users?offset=${page}&limit=10`);
+      // const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${page}`);
       const data = await res.json();
-      setItems([...items, ...data.products]);
-      setPage(prev => prev + 10);
+      setItems([...items, ...data.users]);
+      setPage(prev =>  prev + 10);
+      console.log("page",page);
     } catch(err) {
       console.log("Error", err);
+    }finally {
+      setIsLoading(false);
     }
+  }
+
+
+  function handleScroll() {
+    if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || isLoading) {
+      return;
+    }
+    getItemsList();
   }
 
   useEffect(() => {
     getItemsList();
   }, []);
 
-  function handleScroll() {
-    if( window.innerHeight + window.scrollY >= window.document.body.offsetHeight) {
-      console.log("Running");
-      getItemsList();
-    }
-  }
-
 
   useEffect(() => {
+    console.log("runnign");
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  },[page]);
+  },[isLoading]);
 
   return (
     <div className="App">
       <h2>Infinite Scroll</h2>
       {items.map(each => (
-        <div style={{border: '1px solid black', padding: '24px', margin: '16px'}}>{each.id} --- {each.title} </div>
+        <div key={each.id} style={{border: '1px solid black', padding: '24px', margin: '16px'}}>{each.id} --- {each.first_name} </div>
       ))}
     </div>
   );
